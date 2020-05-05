47ed609ddc68fb263211110f1cdbd4fcbb31807f
Evan Haegele <ehaegele@Evans-MacBook-Air.local>
2020-05-05 15:44:46 -0700

    api changes

--- controllers/resorts.php
+++ controllers/resorts.php
@@ -2,9 +2,7 @@
 include_once __DIR__ . '/../models/resort.php';
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 
 if ($_REQUEST['action'] === 'index') {
   echo json_encode(Resorts::all());
